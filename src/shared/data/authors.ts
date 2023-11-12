5eaeefc83934d648ef0133b5f57f199183284aa8
Нахатакян Артур Романович <artoim333@gmail.com>
2023-11-12 03:18:06 +0300

    refactor

---
+++ src/shared/data/authors.ts
@@ -0,0 +1,32 @@
+export const authors = [
+    {
+        id: 1,
+        name: 'Нахатакян Артур Романович',
+        email: 'aaryan@aaryan.ru',
+        img: '../../../pin.jpeg'
+    },
+    {
+        id: 2,
+        name: 'Спирянов Максим Дмитриевич',
+        email: 'maxim87858@mail.ru',
+        img: '../../../Krosh.jpg'
+    },
+    {
+        id: 3,
+        name: 'Киняев Илья Константинович',
+        email: 'ikinyaev2004@mail.ru',
+        img: '../../../esh.jpg'
+    },
+    {
+        id: 4,
+        name: 'Магер Егор Владимирович',
+        email: 'egor.mager.hl@mail.ru',
+        img: '../../../kopat.jpeg'
+    },
+    {
+        id: 5,
+        name: 'Сурков Андрей Анатольевич',
+        email: 'andrey.surk.2904@gmail.com',
+        img: '../../../los.jpg'
+    },
+]
