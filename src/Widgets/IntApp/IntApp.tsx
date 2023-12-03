95877aa19ade0d01907d6a533af1bae112e16345
Нахатакян Артур Романович <artoim333@gmail.com>
2023-12-03 21:28:49 +0300

    ref: reactored params in intapp

--- src/Widgets/IntApp/IntApp.tsx
+++ src/Widgets/IntApp/IntApp.tsx
@@ -2,9 +2,9 @@ import { DefureEqual } from '../DefureEqual/DefureEqual';
 import { FormParams } from '../../shared/assets/lib/formParams';
 import './IntApp.scss';
 
-const IntApp = ({ postParams, preParams, activeDef, double, thirdSistem }: FormParams) => {
+const IntApp = (params: FormParams) => {
     return <>
-        <DefureEqual postParams={postParams} preParams={preParams} activeDef={activeDef} double={double} thirdSistem={thirdSistem}/>
+        <DefureEqual {...params} />
     </>
 }
 
