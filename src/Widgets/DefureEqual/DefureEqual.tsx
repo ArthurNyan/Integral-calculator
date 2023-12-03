8c0fda4a1142aa73f13cf1618d674d0954f408dc
Нахатакян Артур Романович <artoim333@gmail.com>
2023-12-03 17:57:25 +0300

    feat: add sestem method

--- src/Widgets/DefureEqual/DefureEqual.tsx
+++ src/Widgets/DefureEqual/DefureEqual.tsx
@@ -3,12 +3,12 @@ import { IntegralIcon } from "../../shared/assets/icons/Integral";
 import { RootState } from "../../app/store/store";
 import { updateResult, updateValue } from "../../app/store/intSlice";
 import { FormParams } from "../../shared/assets/lib/formParams";
-import { TableResult } from "../../features/TableResult/TableResult";
+import { SisTableResult, TableResult } from "../../features/TableResult/TableResult";
 import { useState } from "react";
-import { Canvas } from "../../features/Canvas/Canvas";
+import { Canvas, SisCanvas } from "../../features/Canvas/Canvas";
 import { TextField } from "../../features/TextField/TextField";
 
-export const DefureEqual = ({ preParams, postParams, activeDef = false, double = false }: FormParams) => {
+export const DefureEqual = ({ preParams, postParams, activeDef = false, double = false, thirdSistem = false }: FormParams) => {
     const [state, setState] = useState<boolean>();
     const intState = useSelector((state: RootState) => state.intState.value);
     const endpoit = useSelector((state: RootState) => state.pageState.value.endpoit);
@@ -18,7 +18,33 @@ export const DefureEqual = ({ preParams, postParams, activeDef = false, double =
         {preParams && <TextField params={preParams} />}
         <div className="int-base">
             {
-                activeDef ?
+                thirdSistem &&
+                <>
+                    <div className="sitsem">&#9128;</div>
+                    <div>
+                        <div>
+                            <span>dx/dt</span>
+                            <span>=</span>
+                            <input required placeholder="Введите подинтегральное выражение" type="text" name="text" id='equation' className="input"
+                                onChange={() => { dispatch(updateValue({ equation: String((document.getElementById('equation') as HTMLInputElement).value) })) }} />
+                        </div>
+                        <div>
+                            <span>dy/dt</span>
+                            <span>=</span>
+                            <input required placeholder="Введите подинтегральное выражение" type="text" name="text" id='equation2' className="input"
+                                onChange={() => { dispatch(updateValue({ equation2: String((document.getElementById('equation2') as HTMLInputElement).value) })) }} />
+                        </div>
+                        <div>
+                            <span>dz/dt</span>
+                            <span>=</span>
+                            <input required placeholder="Введите подинтегральное выражение" type="text" name="text" id='equation3' className="input"
+                                onChange={() => { dispatch(updateValue({ equation3: String((document.getElementById('equation3') as HTMLInputElement).value) })) }} />
+                        </div>
+                    </div>
+                </>
+            }
+            {
+                !thirdSistem && (activeDef ?
                     <>
                         {double && <span>d</span>}
                         <span>d(y)</span>
@@ -27,15 +53,18 @@ export const DefureEqual = ({ preParams, postParams, activeDef = false, double =
                     <>
                         <IntegralIcon />
                         {double && <IntegralIcon />}
-                    </>
+                    </>)
             }
-            <input required placeholder="Введите подинтегральное выражение" type="text" name="text" id='equation' className="input"
-                onChange={() => { dispatch(updateValue({ equation: String((document.getElementById('equation') as HTMLInputElement).value) })) }} />
             {
-                !activeDef && <>
+                !thirdSistem &&
+                <input required placeholder="Введите подинтегральное выражение" type="text" name="text" id='equation' className="input"
+                    onChange={() => { dispatch(updateValue({ equation: String((document.getElementById('equation') as HTMLInputElement).value) })) }} />
+            }
+            {
+                !thirdSistem && (!activeDef && <>
                     <span>=</span>
                     <div id='result' className="inp-3">{intState.result}</div>
-                </>
+                </>)
             }
         </div>
         {
@@ -53,6 +82,12 @@ export const DefureEqual = ({ preParams, postParams, activeDef = false, double =
             activeDef && state && <>
                 <TableResult />
                 <Canvas />
+            </>
+        }
+        {
+            thirdSistem && state && <>
+                <SisTableResult />
+                <SisCanvas />
             </>
         }
     </div>
